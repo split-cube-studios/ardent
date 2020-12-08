add5fa99aeac33d60b381acf35a51db00bafa4d8
Zachary Burkett <zburkett@splitcubestudios.com>
2020-12-08 08:56:11 -0500

    WIP Spatial Partioning

--- engine/context.go
+++ engine/context.go
@@ -8,16 +8,23 @@ type Context struct {
 	Renderer
 	Collider
 
+	partitionMap *PartitionMap
+
 	entities   map[string][]Entity
 	entitySwap []Entity
 }
 
 func NewContext(renderer Renderer, collider Collider) *Context {
-	return &Context{
+	ctx := &Context{
 		Renderer: renderer,
 		Collider: collider,
 		entities: make(map[string][]Entity),
 	}
+
+	// TODO configurable values
+	ctx.partitionMap = NewPartitionMap(250, 1000)
+
+	return ctx
 }
 
 func (c *Context) AddEntity(entities ...Entity) {
@@ -35,75 +42,58 @@ func (c *Context) Tick() {
 		}
 		c.AddImage(e.Images()...)
 
-		c.entities[e.Class()] = append(
-			c.entities[e.Class()],
-			e,
-		)
+		c.partitionMap.Add(e)
 
 		c.entitySwap[i] = nil
 	}
 	c.entitySwap = c.entitySwap[:0]
 
-	for class, entities := range c.entities {
-		var i int
-		for _, e := range entities {
-			if e.IsDisposed() {
-				continue
-			}
+	vp := c.Viewport()
+	pos := Vec2{
+		float64(vp.Min.X),
+		float64(vp.Min.Y),
+	}
+	c.partitionMap.Tick(pos, 1, c.updateEntities)
+}
 
-			if c.Cull(e.Position()) {
-				c.entities[class][i] = e
-				i++
+func (c *Context) updateEntities(entries []PartitionEntry) {
+	for _, entry := range entries {
+		entry.(Entity).Tick()
 
-				continue
-			}
+		hitbox, ok := entry.(Hitbox)
+		if !ok {
+			return
+		}
 
-			// TODO make this good
-			// spacial partitioning, etc
-			if hitbox, ok := e.(Hitbox); ok {
-				for _, targetClass := range hitbox.HitClasses() {
-					for _, targetEntity := range c.entities[targetClass] {
-						targetHitbox, ok := targetEntity.(Hitbox)
-						if !ok {
-							continue
-						}
+		for _, targetClass := range hitbox.HitClasses() {
+			entries := c.partitionMap.Class(targetClass)
+			for _, targetEntry := range entries {
+				targetHitbox, ok := targetEntry.(Hitbox)
+				if !ok {
+					continue
+				}
 
-						if c.Cull(targetEntity.Position()) {
-							continue
-						}
+				sourcePos := hitbox.Position()
+				sourceBounds := hitbox.HitBounds()
+				targetPos := targetHitbox.Position()
+				targetBounds := targetHitbox.HitBounds()
 
-						sourcePos := hitbox.Position()
-						sourceBounds := hitbox.HitBounds()
-						targetPos := targetHitbox.Position()
-						targetBounds := targetHitbox.HitBounds()
-
-						if image.Rect(
-							int(sourcePos.X)+sourceBounds.Min.X,
-							int(sourcePos.Y)+sourceBounds.Min.Y,
-							int(sourcePos.X)+sourceBounds.Max.X,
-							int(sourcePos.Y)+sourceBounds.Max.Y,
-						).Overlaps(
-							image.Rect(
-								int(targetPos.X)+targetBounds.Min.X,
-								int(targetPos.Y)+targetBounds.Min.Y,
-								int(targetPos.X)+targetBounds.Max.X,
-								int(targetPos.Y)+targetBounds.Max.Y,
-							),
-						) {
-							hitbox.Hit(targetEntity)
-						}
-					}
+				if image.Rect(
+					int(sourcePos.X)+sourceBounds.Min.X,
+					int(sourcePos.Y)+sourceBounds.Min.Y,
+					int(sourcePos.X)+sourceBounds.Max.X,
+					int(sourcePos.Y)+sourceBounds.Max.Y,
+				).Overlaps(
+					image.Rect(
+						int(targetPos.X)+targetBounds.Min.X,
+						int(targetPos.Y)+targetBounds.Min.Y,
+						int(targetPos.X)+targetBounds.Max.X,
+						int(targetPos.Y)+targetBounds.Max.Y,
+					),
+				) {
+					hitbox.Hit(targetEntry.(Entity))
 				}
 			}
-
-			e.Tick()
-			c.entities[class][i] = e
-			i++
 		}
-
-		for j := i; j < len(c.entities[class]); j++ {
-			c.entities[class][j] = nil
-		}
-		c.entities[class] = c.entities[class][:i]
 	}
 }
