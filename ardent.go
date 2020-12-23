9240704fb338cb943529ef44996747c13344fe88
Terra Brown <superloach42@gmail.com>
2020-12-23 09:20:13 -0500

    tweak docs

--- ardent.go
+++ ardent.go
@@ -4,9 +4,8 @@ import "github.com/split-cube-studios/ardent/engine"
 
 // NewGame creates a new game instance.
 //
-// The backend can be selected with one of the following build tags:
-//  ebiten
-//  headless
+// The backend can be selected with build tags.
+// The default, with no tag, is to use Ebiten.
 func NewGame(
 	title string,
 	w, h int,
