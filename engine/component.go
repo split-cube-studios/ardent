a16731dfb1560accf44334c87e4b01f86fa450fc
Zachary Burkett <zburkett@splitcubestudios.com>
2021-06-26 10:57:07 -0400

    Refactor component interfaces

--- engine/component.go
+++ engine/component.go
@@ -9,24 +9,48 @@ import (
 
 // Component produces engine components.
 type Component interface {
-	NewAssetFromPath(string) (Asset, error)
+	AssetComponent
+	ImageComponent
+	SoundComponent
+	RendererComponent
+	CameraComponent
+	PhysicsComponent
+}
 
+// AssetComponent produces asset components.
+type AssetComponent interface {
+	NewAssetFromPath(string) (Asset, error)
+	ImageComponent
+	SoundComponent
+}
+
+// ImageComponent produces image components.
+type ImageComponent interface {
 	NewImageFromPath(string) (Image, error)
 	NewImageFromAssetPath(string) (Image, error)
 	NewImageFromImage(image.Image) Image
-
 	NewTextImage(string, int, int, font.Face, color.Color) Image
-
 	NewAtlasFromAssetPath(string) (Atlas, error)
-
 	NewAnimationFromAssetPath(string) (Animation, error)
+}
 
+// SoundComponent produces sound components.
+type SoundComponent interface {
 	NewSoundFromAssetPath(string) (Sound, error)
+}
 
+// RendererComponent produces renderer components.
+type RendererComponent interface {
 	NewRenderer() Renderer
 	NewIsoRenderer() IsoRenderer
+}
 
+// CameraComponent produces camera components.
+type CameraComponent interface {
 	NewCamera() Camera
+}
 
+// PhysicsComponent produces physics components.
+type PhysicsComponent interface {
 	NewCollider() Collider
 }
